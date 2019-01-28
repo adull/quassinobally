c377de0945e6b4b807bf1a3b82c50c621e32e412
adull <adlaiabdelrazaq@gmail.com>
2019-01-27 20:50:26 -0600

    ran prod build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2adb03ec3f9179a23e630dbd3c7ae3d5.js"
+  "/quassinobally/precache-manifest.19c6b6129bc8140b836537ce2d90837e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/quassinobally/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
