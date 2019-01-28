334d835c10997a89bccd8bc22a32556123029b95
adull <adlaiabdelrazaq@gmail.com>
2019-01-27 20:35:50 -0600

    added build

---
+++ build/precache-manifest.2adb03ec3f9179a23e630dbd3c7ae3d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fdf7990f3a129906b953",
+    "url": "/static/js/main.fdf7990f.chunk.js"
+  },
+  {
+    "revision": "63d215c769aa6ffee93f",
+    "url": "/static/js/1.63d215c7.chunk.js"
+  },
+  {
+    "revision": "fdf7990f3a129906b953",
+    "url": "/static/css/main.9cd02f34.chunk.css"
+  },
+  {
+    "revision": "5ed7b31d6f9a4c0aea50bcd2a0a2d72f",
+    "url": "/index.html"
+  }
+];
